78b1ad01cdd43ae71417af7e03085bb849546cf4
Matthew Menna <mattmenna@gmail.com>
2017-07-07 10:18:40 -0400

    Initial Commit

---
+++ src/LowestArrayNumber.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Matt on 7/7/2017.
+ */
+public class LowestArrayNumber {
+    public static void main(String[] args) {
+        int[] array1 = {100,56,22,50,10,63,15};
+
+        int highNum=100;
+
+        for (int i = 0; i < array1.length ; i++) {
+            if(array1[i]<highNum){
+                highNum = array1[i];
+            }
+        }
+        System.out.println(highNum);
+
+    }
+}
