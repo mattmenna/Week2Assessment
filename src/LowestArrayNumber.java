0e77b0d5b6ef19d10f0e9fe8c1027f9acb8c9360
Matthew Menna <mattmenna@gmail.com>
2017-07-07 10:20:58 -0400

    Initial Commit

--- src/LowestArrayNumber.java
+++ src/LowestArrayNumber.java
@@ -3,12 +3,12 @@
  */
 public class LowestArrayNumber {
     public static void main(String[] args) {
-        int[] array1 = {100,56,22,50,10,63,15};
+        int[] array1 = {100, 56, 22, 50, 10, 63, 15};
 
-        int highNum=100;
+        int highNum = 100;
 
-        for (int i = 0; i < array1.length ; i++) {
-            if(array1[i]<highNum){
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] < highNum) {
                 highNum = array1[i];
             }
         }
